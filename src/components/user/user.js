0ad95e18998ed6e7d23b4b14c73238060475a874
SungYeon Hwang <hsy0324@khu.ac.kr>
2021-06-09 00:35:23 +0900

    first commit

---
+++ src/components/user/user.js
@@ -0,0 +1,93 @@
+import styled from 'styled-components';
+import {
+  ProSidebar,
+  SidebarHeader,
+  SidebarFooter,
+  SidebarContent,
+  Menu,
+  MenuItem,
+  SubMenu,
+} from 'react-pro-sidebar';
+import { OutlineButton } from '../../styles/GlobalStyle';
+import AsideNavbar from '../../components/asidenav/AsideNavbar';
+import DropdownMenu from '../../components/dropdownMenu/DropdownMenu';
+import React, { handleClick } from 'react';
+import useDropdownMenu from 'react-accessible-dropdown-menu-hook';
+import 'react-pro-sidebar/dist/css/styles.css';
+import TitleImg from '../../images/title-search.jpeg';
+import HashtagImg from '../../images/hashtag-search.jpeg';
+import HumanImg from '../../images/human-search.jpeg';
+import CloudImg from '../../images/cloud-computing.jpeg';
+import GroupImg from '../../images/group.jpeg';
+import {
+  UserContainer,
+  UserWrapper,
+  UserAside,
+  UserSearchForm,
+  UserTitle,
+  UserSearchInput,
+  UserSearchBtn,
+  UserSubTitle,
+  UserContentContainer,
+  UserTabContainer,
+  UserBtn,
+  UserCardWrapper,
+  UserFeature,
+  UserFeatureContent,
+  UserFeatureTitle,
+  UserFeatureText,
+  UserFeatureDetails,
+  UserFeatureItem,
+  UserItemTitle,
+  UserItemContent,
+  UserItemIcon,
+  UserItemText,
+  UserCardSection,
+  UserSmallCards,
+  UserCard,
+  UserCardContent,
+  UserCardHeading,
+  UserCardDetails,
+  UserCardItems,
+  UserCardTitle,
+  UserCardItem,
+  UserCardIcon,
+  UserCardText,
+  UserImg,
+  Img,
+} from './user.styles';
+const Wrapper = styled.div`
+  display: flex;
+  position: sticky;
+  flex-direction: column;
+  column: 100%;
+`;
+const Users = () => (
+  <Wrapper>
+    <UserAside>
+      <UserWrapper>
+        <AsideNavbar />
+      </UserWrapper>
+      <UserWrapper>
+        <UserContainer>
+          <UserTitle>내 드라이브</UserTitle>
+          <UserSearchForm>
+            <UserSearchInput
+              name="search"
+              id="search"
+              type="search"
+              placeholder="드라이브에서 검색"
+            />
+            <UserSearchBtn>Submit</UserSearchBtn>
+          </UserSearchForm>
+        </UserContainer>
+        <UserWrapper>
+          <UserContainer>
+            <UserSubTitle>클라우드 자료</UserSubTitle>
+          </UserContainer>
+        </UserWrapper>
+      </UserWrapper>
+    </UserAside>
+  </Wrapper>
+);
+export default Users;
---
+++ src/pages/UserCloudPage/UserCloudPage.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import styled from 'styled-components';
+import Navbar from '../../components/nav/Navbar';
+import Header from '../../components/nav/Navbar';
+import User from '../../components/user/user';
+import Footer from 'components/footer/footer';
+import HeaderContainer from '../../container/auth/HeaderContainer';
+const Wrapper = styled.div``;
+const UserCloudPage: React.FC = () => {
+  return (
+    <Wrapper>
+      <Header />
+      {/* <Navbar /> */}
+      {/* <HeaderContainer /> */}
+      <User />
+      <Footer />
+    </Wrapper>
+  );
+};
+export default UserCloudPage;
